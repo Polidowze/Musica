dded9b05f6223e7549434f470a7f70d53943d976
polidow <gabriel.polido01@etec.sp.gov.br>
2024-03-15 18:19:31 -0300

    Musica

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine(@"
+Todos os dias quando acordo
+Não tenho mais
+O tempo que passou
+Mas tenho muito tempo
+Temos todo o tempo do mundo
+Todos os dias
+Antes de dormir
+Lembro e esqueço
+Como foi o dia
+Sempre em frente
+Não temos tempo a perder
+Nosso suor sagrado
+É bem mais belo
+Que esse sangue amargo
+E tão sério");
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine(@"
+E selvagem! Selvagem!
+Selvagem!");
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine(@"Veja o sol
+Dessa manhã tão cinza
+A tempestade que chega
+É da cor dos teus olhos
+Castanhos
+Então me abraça forte
+E diz mais uma vez
+Que já estamos
+Distantes de tudo");
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine(@"
+Temos nosso próprio tempo
+Temos nosso próprio tempo
+Temos nosso próprio tempo");
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine(@"
+Não tenho medo do escuro
+Mas deixe as luzes
+Acesas agora
+O que foi escondido
+É o que se escondeu
+E o que foi prometido
+Ninguém prometeu
+Nem foi tempo perdido
+Somos tão jovens
+Tão jovens! Tão jovens!");
+Console.ResetColor();
